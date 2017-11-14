7c1d1ee6444e8c2da4ba80a4555caae584fce160
nikit91@gmail.com <nikit91@gmail.com>
2017-11-15 00:44:59 +0100

    Adding kg asgn1

---
+++ KG_Asgn01/src/edu/upb/kg/ConvertToRDF.java
@@ -0,0 +1,66 @@
+package edu.upb.kg;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.text.StrSubstitutor;
+
+public class ConvertToRDF {
+
+	public static final String RD_PREFIX = "@prefix rdf:\t<http://www.w3.org/1999/02/22-rdf-syntax-ns#>.";
+	public static final String DBP_PREFIX = "@prefix ex:\t<http://dbpedia.org/ontology/>.";
+	public static final String MAIN_URI_TMPL = "<http://dbpedia.org/ontology/${propName}#${value}>";
+	public static final String DATA_TMPL = "ex:${propName} \"${value}\" ";
+
+	public static void main(String[] args) throws IOException {
+		Path path = Paths.get(args[0]);
+		List<String> entryList = new ArrayList<String>();
+		Files.lines(path).forEachOrdered(s -> entryList.add(s));
+		String[] predicates = entryList.get(0).split("\\t");
+		int predLen = predicates.length;
+		// Generate main URI template
+		String mainPropName = predicates[0];
+		List<String> outputList = new ArrayList<String>();
+		outputList.add(RD_PREFIX);
+		outputList.add(DBP_PREFIX);
+		String[] curEntries;
+		Map<String, String> valuesMap = new HashMap<String, String>();
+		valuesMap.put("propName", mainPropName);
+
+		StrSubstitutor sub;
+		for (int i = 1; i < entryList.size(); i++) {
+			curEntries = entryList.get(i).split("\\t");
+			valuesMap.put("value", curEntries[0]);
+			sub = new StrSubstitutor(valuesMap);
+			for (int j = 1; j < predLen; j++) {
+				// if condition for mainProp
+				if (j == 1)
+					outputList.add(sub.replace(MAIN_URI_TMPL));
+				// normal prop append
+				else
+					outputList.add(getPropStr(predicates[j], curEntries[j], j == predLen - 1));
+			}
+		}
+		
+		for(String x: outputList) {
+			System.out.println(x);
+		}
+	}
+
+	public static String getPropStr(String propName, String value, boolean isLast) {
+		String propStr = null;
+		Map<String, String> valuesMap = new HashMap<String, String>();
+		valuesMap.put("propName", propName);
+		valuesMap.put("value", value);
+		StrSubstitutor sub = new StrSubstitutor(valuesMap);
+		propStr = sub.replace(DATA_TMPL);
+		return "\t"+propStr+(isLast?" .":" ;");
+	}
+
+}
